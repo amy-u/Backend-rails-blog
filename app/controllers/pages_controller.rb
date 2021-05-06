59ad4cfa76893fb64ae71fc4e3d27c71c21b3afe
Amy Ung <amyung.lm@gmail.com>
2021-05-05 23:20:37 -0700

    Created new method for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,9 @@ class PagesController < ApplicationController
     def show
         @page = Page.find(params[:id])
     end
+
+    def new
+        @page = Page.new
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/pages', to: 'pages#index'
+  # pages/new needs to be before id
+  get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
+
 end
