5437e9377714978ccdf136136cf750645601ee6f
Amy Ung <amyung.lm@gmail.com>
2021-05-06 00:37:35 -0700

    modularize code in controller and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
+    before_action :set_page, only: [:show, :edit, :update, :destory]
+
     def index
         @pages = Page.all
     end
 
     def show
-        @page = Page.find(params[:id])
+        set_page
     end
 
     def new
@@ -18,17 +20,17 @@ class PagesController < ApplicationController
     end
 
     def edit
-        @page = Page.find(params[:id])
+        set_page
     end
 
     def update
-        @page = Page.find(params[:id])
+        set_page
         @page.update(page_params)
         redirect_to @page
     end
 
     def destroy
-        @page = Page.find(params[:id])
+        set_page
         @page.destroy
         redirect_to pages_path
     end
@@ -39,4 +41,7 @@ class PagesController < ApplicationController
         params.require(:page).permit(:title, :body, :slug)
     end
 
+    def set_page
+        @page = Page.find(params[:id])
+    end
 end
