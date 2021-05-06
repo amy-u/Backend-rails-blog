bf29d8b84db7be0f082670513de4159a5465fa0f
Amy Ung <amyung.lm@gmail.com>
2021-05-06 00:10:50 -0700

    partial the form and updated the edit to connect in views/controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # pages/new needs to be before id
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
+  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
 end
